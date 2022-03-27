29467b8967e2510dbbbb9e34c998370ed160b5f3
吕同学 <1422675202@qq.com>
2022-03-27 15:07:40 +0800

    add: 用时击败87.10%，内存消耗击败83.53%%

---
+++ 敲力扣/面试特定练习题/LC43字符串相乘_开个整型数组模拟乘法过程.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+//开个整型数组模拟乘法过程
+//执行用时：4 ms, 在所有 C++ 提交中击败了87.10%的用户
+//内存消耗：6.5 MB, 在所有 C++ 提交中击败了83.53%的用户
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1=="0"||num2=="0")
+            return "0";
+        int len1 = num1.length();
+        int len2 = num2.length();
+
+        int* ans=new int[len1 + len2];
+        for(int i=0;i<len1+len2;i++)
+            ans[i]=0;
+
+        int i, j;
+        for (i = len1 - 1; i >= 0; --i) {
+            for (j = len2 - 1; j >= 0; --j) {
+                int num = (num1[i]-48) * (num2[j] - 48) + ans[i+j+1];
+                ans[i+j] += num / 10;
+                ans[i+j+1] = num % 10;
+            }
+        }
+
+        string sans;
+        for(i=0;i<len1+len2;i++){
+            if(ans[i]==0&&i==0)
+                continue;
+            sans+=ans[i]+48;
+        }
+        return sans;
+    }
+
+};
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    //string ans(1+1+1,'0');
+    Solution sol;
+    string ans = sol.multiply(s1, s2);
+    cout << ans << endl;
+    return 0;
+}
